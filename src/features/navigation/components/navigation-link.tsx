cb593500ab37a4b2e73bb0fdec8cf996ffcc8eae
mikuull <mmikulski643@gmail.com>
2024-12-29 21:26:37 +0100

    feat: implemented scroll to path logic

--- src/features/navigation/components/navigation-link.tsx
+++ src/features/navigation/components/navigation-link.tsx
@@ -1,6 +1,16 @@
+"use client";
+import { scrollToElement } from "@/utils/scroll";
 import React from "react";
 
 export const NavigationLink = ({ name, path }: NavigationLinkType) => {
-  //TODO: scroll to path implementation
-  return <p className="cursor-pointer text-[#979797] font-semibold">{name}</p>;
+  return (
+    <p
+      className="cursor-pointer text-[#979797] font-semibold"
+      onClick={(e) => {
+        scrollToElement(e, path, 80);
+      }}
+    >
+      {name}
+    </p>
+  );
 };
