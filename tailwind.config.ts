4df6b3d4197239c6fa985f3bca494399d4a637af
mikuull <mmikulski643@gmail.com>
2024-12-29 16:15:53 +0100

    chore: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,16 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        purplebg: "#14142b",
+        main: "#7b4ae2;",
+        bg: "#0C0F17",
       },
     },
   },
