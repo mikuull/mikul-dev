888b2b2c3e2dfd561151d3fbaaf7e02366bb00ce
mikuull <mmikulski643@gmail.com>
2024-12-29 21:17:59 +0100

    feat: added skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import { About } from "@/features/about/components/about";
 import { Header } from "@/features/header/components/header";
 import { Navigation } from "@/features/navigation/components/navigation";
 import { Projects } from "@/features/project/components/projects";
+import { Skills } from "@/features/skills/components/skills";
 
 export default function Home() {
   return (
     <>
-      <main className="">
+      <main>
         <Navigation />
         <div className="divide-y divide-purplebg">
           <Header />
           <About />
           <Projects />
+          <Skills />
         </div>
       </main>
     </>
