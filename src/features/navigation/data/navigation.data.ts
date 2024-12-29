2b697b3f01434a62912612f33966baca18c0c49c
mikuull <mmikulski643@gmail.com>
2024-12-29 16:17:10 +0100

    feat: added navigation link component

---
+++ src/features/navigation/data/navigation.data.ts
@@ -0,0 +1,24 @@
+export const navigationLinks: NavigationLinkType[] = [
+  {
+    name: "Home",
+    path: "#home",
+  },
+  {
+    name: "About me",
+    path: "#aboutme",
+  },
+
+  {
+    name: "Projects",
+    path: "#projects",
+  },
+
+  {
+    name: "Skills",
+    path: "#skills",
+  },
+  {
+    name: "Contact",
+    path: "#contact",
+  },
+];
