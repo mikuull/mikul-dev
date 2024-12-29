ad3dcd62503d3647b10e1b4d608507b6b5de6cfd
mikuull <mmikulski643@gmail.com>
2024-12-29 21:54:59 +0100

    feat: added footer section

---
+++ src/features/footer/components/footer.tsx
@@ -0,0 +1,13 @@
+import { HeaderIcons } from "@/features/header/components/header-icons";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <section className="py-20 flex justify-between items-center px-20">
+      <p className="text-lg font-semibold text-[#797979]">
+        Copyright © Mateusz Mikulski · 2024
+      </p>
+      <HeaderIcons />
+    </section>
+  );
+};
